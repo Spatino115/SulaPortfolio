6c9871e0689f2cad14ff9bb78482672ff358d098
Spatino115 <sapatino115@gmail.com>
2024-11-25 16:05:17 -0500

    Delete interaction.js

--- interaction.js
+++
@@ -1,6 +0,0 @@
-const cursor = document.getElementById("custom-cursor");
-
-document.addEventListener("mousemove", (event) => {
-  cursor.style.left = event.clientX - 15 + "px";
-  cursor.style.top = event.clientY - 15 + "px";
-});
