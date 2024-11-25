50ae2def2f91fe1c39de71ec00ce9bd997ca5ebe
Spatino115 <sapatino115@gmail.com>
2024-11-25 14:33:47 -0500

    Update interaction.js

--- interaction.js
+++ interaction.js
@@ -1,6 +1,6 @@
-const cursor = document.getElementById('custom-cursor');
+const cursor = document.getElementById("custom-cursor");
 
-document.addEventListener('mousemove', (event) => {
-  cursor.style.left = event.clientX - cursor.offsetWidth / 2 + 'px';
-  cursor.style.top = event.clientY - cursor.offsetHeight / 2 + 'px';
+document.addEventListener("mousemove", (event) => {
+  cursor.style.left = event.clientX - 15 + "px";
+  cursor.style.top = event.clientY - 15 + "px";
 });
